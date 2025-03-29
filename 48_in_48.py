9f0f58f756c8a5dd54bbf79e9d5ba5b9eeb729e0
fmr2689 <fmr2689@rit.edu>
2025-03-29 14:48:30 -0400

    Added support for new folks btwn thurs-fri

--- 48_in_48.py
+++ 48_in_48.py
@@ -19,7 +19,6 @@ saturday_fnames = []
 thursday_lnames = [] 
 saturday_lnames = [] 
 
-line = 0 
 header = next(saturday_reader)
 header = next(thursday_reader)
 for row in thursday_reader: 
@@ -32,31 +31,41 @@ for row in saturday_reader:
     saturday_lnames.append(row[3])
     saturday_numbers.append(row[10])
 
-'''
-abc = 0 
-for abc in range(10): 
-    print(thursday_fnames[abc])
-    print(friday_fnames[abc])
-    print(thursday_lnames[abc])
-    print(friday_lnames[abc])
-    print(thursday_numbers[abc])
-    print(friday_numbers[abc])
-    print(str(float(friday_numbers[abc]) - float(thursday_numbers[abc])))'
-'''
-
 x = 0
-for i in range(len(thursday_fnames)): 
-    if thursday_fnames[x] != saturday_fnames[x] or thursday_lnames[x] != saturday_lnames[x]: 
-        print("you fucked up")
+for i in range(len(saturday_fnames)): 
+    if x < len(thursday_fnames) and (thursday_fnames[x] != saturday_fnames[i] or thursday_lnames[x] != saturday_lnames[i]):
+        print(f"Mismatch or new entry found: {saturday_fnames[i]} {saturday_lnames[i]}")
+    elif x >= len(thursday_fnames): 
+        print(f"New entry found: {saturday_fnames[i]} {saturday_lnames[i]}")
+        '''
+        if float(saturday_numbers[x]) - float(thursday_numbers[x]) >= 48.00: 
+            if thursday_fnames[x] == saturday_fnames[x] and thursday_lnames[x] == saturday_lnames[x]: 
+                name = thursday_fnames[x] + thursday_lnames[x] 
+                winners.append(name)'
+        '''
     x += 1 
 
 y = 0 
-for j in range(len(thursday_numbers)): 
+for j in range(len(saturday_numbers)): 
+    if y < len(thursday_numbers) and float(saturday_numbers[j]) - float(thursday_numbers[y]) >= 48.00: 
+        if thursday_fnames[y] == saturday_fnames[j] and thursday_lnames[y] == saturday_lnames[j]: 
+            name = thursday_fnames[y] + " " + thursday_lnames[y] 
+            winners.append(name)
+    elif y >= len(thursday_numbers):  # Handle new entries in Saturday report
+        if float(saturday_numbers[j]) >= 48.00: 
+            name = saturday_fnames[j] + " " + saturday_lnames[j]
+            winners.append(name)
+    y += 1 
+
+'''
+y = 0 
+for j in range(len(saturday_numbers)): 
     if float(saturday_numbers[y]) - float(thursday_numbers[y]) >= 48.00: 
         if thursday_fnames[y] == saturday_fnames[y] and thursday_lnames[y] == saturday_lnames[y]: 
             name = thursday_fnames[y] + thursday_lnames[y] 
             winners.append(name)
     y += 1 
+'''
 
 for name in winners: 
     print(name)
